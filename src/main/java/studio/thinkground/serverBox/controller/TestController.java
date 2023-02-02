0f3c47d1749912fb9fa47a4c73b4539ea1d8ea54
YeJi222 <70511859+YeJi222@users.noreply.github.com>
2023-02-02 16:14:46 +0900

    first commit

---
+++ src/main/java/studio/thinkground/serverBox/controller/TestController.java
@@ -0,0 +1,53 @@
+package studio.thinkground.serverBox.controller;
+
+import org.apache.coyote.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import studio.thinkground.serverBox.dto.MemberDTO;
+
+@RestController
+@RequestMapping("/api/server")
+public class TestController {
+    private final Logger LOGGER = LoggerFactory.getLogger(TestController.class);
+
+    @GetMapping(value = "/around-hub")
+    public String getTest1(){
+        LOGGER.info("getTest1 호출!");
+        return "Hello. Around Hub Studio!";
+    }
+
+    @GetMapping(value = "/name")
+    public String getTest2(@RequestParam String name){
+        LOGGER.info("getTest2 호출!");
+        return "Hello. " + name + "!";
+    }
+
+    @GetMapping(value = "/member")
+    public ResponseEntity<MemberDTO> getMember(
+            @RequestBody MemberDTO memberDTO,
+            @RequestParam String name,
+            @RequestParam String email,
+            @RequestParam String organization
+    ){
+        LOGGER.info("getMember 호출!");
+
+        /*
+        MemberDTO memberDTO1 = new MemberDTO();
+        memberDTO1.setName(name);
+        memberDTO1.setOrganization(organization);
+         */
+
+        return ResponseEntity.status(HttpStatus.OK).body(memberDTO);
+    }
+
+    @PostMapping(value = "/add-header")
+    public ResponseEntity<MemberDTO> addHeader(@RequestHeader("around-header") String header, @RequestBody MemberDTO memberDTO){
+        LOGGER.info("add-header 호출!");
+        LOGGER.info("header 값 : {}", header);
+
+        return ResponseEntity.status(HttpStatus.OK).body(memberDTO);
+    }
+}
